e22924b199ef1313d6b395df8209f34eb11ce3bb
Laurice Howard <lowkeylaurice@gmail.com>
2022-07-21 13:09:24 -0400

    register route fully built out

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,4 +1,5 @@
 const { client_url, jwtSecret } = require("../config/index");
+const { UniqueConstraintError } = require("sequelize/lib/errors");
 const Express = require("express");
 const router = Express.Router();
 const { Admin } = require("../models");
@@ -7,10 +8,34 @@ const bcrypt = require("bcryptjs");
 router.post("/register", async (req, res) => {
   const { email, password } = req.body;
   const pwdHash = bcrypt.hashSync(password, 13);
-  await Admin.create({
+  try {
+    if (email === "v@vtingz.com"){
+  const createAdmin = await Admin.create({
     emailAddress: email,
     passwordHash: pwdHash,
   });
+  if (createAdmin.emailAddress === "v@vtingz.com"){
+    res.status(201).json({
+        message: "Admin successfully registered",
+        user: createAdmin
+    })
+  } else {
+    res.status(401).json({
+        message: "Login failed",
+    })
+  }
+}
+} catch (err) {
+    if (err instanceof UniqueConstraintError) {
+        res.status(409).json({
+            message: "Email already in use",
+        }); 
+    } else {
+        res.status(500).json({
+            message: "Failed to register admin",
+        });
+    }
+}
   res.json()
 });
 
