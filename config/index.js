0953c17fd23903fc1362bb1d1cca74d25fa97136
Laurice Howard <lowkeylaurice@gmail.com>
2023-02-12 17:46:06 -0500

    pushing changes for server

--- config/index.js
+++ config/index.js
@@ -1,18 +1,23 @@
 require("dotenv").config();
 
 const config = {
-    appPort: parseInt(process.env.PORT),
-    dbName: process.env.DB_NAME,
-    dbURL: `postgres://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.DB_HOST}:${parseInt(process.env.DB_PORT)}/${process.env.DB_NAME}`,
-    stripe: require("stripe")(`${process.env.SECRET_KEY}`),
-    jwtSecret: process.env.JWT_KEY,
-    client_url: process.env.CLIENT_URL,
-    webhook_secret: process.env.WEB_HOOK_SECRET,
-    sender: process.env.SENDER,
-    host: process.env.EMAIL_HOST,
-    pass: process.env.PASSWORD,
-    admin_email: process.env.ALLOWED_EMAIL,
-    dev_email: process.env.OTHER_ALLOWED_EMAIL,
+  appPort: parseInt(process.env.PORT),
+  dbName: process.env.DB_NAME,
+  dbURL:
+    process.env.NODE_ENV === "production"
+      ? process.env.DATABASE_URL
+      : `postgres://${process.env.DB_USER}:${process.env.DB_PWD}@${
+          process.env.DB_HOST
+        }:${parseInt(process.env.DB_PORT)}/${process.env.DB_NAME}`,
+  stripe: require("stripe")(`${process.env.SECRET_KEY}`),
+  jwtSecret: process.env.JWT_KEY,
+  client_url: process.env.CLIENT_URL,
+  webhook_secret: process.env.WEB_HOOK_SECRET,
+  sender: process.env.SENDER,
+  host: process.env.EMAIL_HOST,
+  pass: process.env.PASSWORD,
+  admin_email: process.env.ALLOWED_EMAIL,
+  dev_email: process.env.OTHER_ALLOWED_EMAIL,
 };
 
 module.exports = config;
